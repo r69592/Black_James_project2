cedf43a5186f8c30f0479f83ccb14d949a26d0bc
JamesOBlack <jamesoblack@fullsail.edu>
2013-05-12 11:08:31 -0400

    adding show data function, save data function and get id function.

--- js/main.js
+++ js/main.js
@@ -5,20 +5,81 @@ VFW : 1305
 Project 2
 
 */
+window.addEventListener("DOMContentLoaded", function(){
+    
+    // Clear storage.
+        var storage   = $("clearStorage");
+        storage.addEventListener("click", clearStorage);
+    
+     var clearStorage = function() {
+        //confirm("Do you want to clear all data?")
+        localStorage.clear();
+        console.log("Clear worked");
+    }
+    
+    
+    
+    
+    
+    // Show storage.
+        var showStorage = $("displayData");
+        showStorage.addEventListener("click", showData);
+        
+        function showData(){
+            var makeDiv = document.createElement("div");
+            makeDiv.setAttribute("id", "items");
+            var makeList = document.createElement("ul");
+            makeDiv.appendChild(makeList);
+            document.body.appendChild(makeDiv);
+            for (var i=0, len=localStorage.length; i,len; i++) {
+                 var makeLi = document.createElement("li");
+                 makeList.appendChild(makeLi);
+                 var key = localStorage.key(i);
+                 var value = localStorage.getItem(key);
+                 var obj = JSON.parse(value);
+                 var makeSublist = document.createElement("ul");
+                 makeLi.appendChild(makeSublist);
+                 for (var n in obj) {
+                      var makeSubli = document.createElement("li");
+                      makeSublist.appendChild(makeSubli);
+                      var optSubText = obj[n][0]+" "+obj[n][1];
+                      makeSubli.innerHTML = optSubText;
+                 }
+            }
+        }
+    
+    // Save data
+        var saveButton     = $("saveButton");
+        saveButton.addEventListener("click", saveData);
+        function saveData() {
+        var id             = Math.floor(Math.random()*1000001);
+        var item           = {}
+            item.chore     = ["Chore", $("chore").value];
+            item.type      = ["Type", $("type").value];
+            //item.easy      = ["Easy", $("easy").value];
+            //item.medium    = ["Medium", $("medium").value];
+            //item.hard      = ["Hard", $("hard").value];
+            item.range     = ["Range", $("range").value];
+            item.choreDate = ["Chore Date", $("choreDate").value];
+            item.notes     = ["Notes", $("notes").value];
+            localStorage.setItem(id, JSON.stringify(item));
+            alert("Chore Saved!");
+    }
 
-var save = document.getElementById("button");
-var textField= document.getElementById("chore");
+    
+    // My get id function.
+    function $(x) {
+        var elementId = document.getElementById(x);
+        return elementId;
+    }
+    
+    
+    
+   
+    //localStorage.clear();
+    
+    
 
-var changeBorder = function() {
- textField.setAttribute("class", "hasFocus")    
-}
-var normalBorder = function() {
- textField.removeAttribute("class", "hasFocus")    
-}
-
-textField.addEventListener("focus",changeBorder);
-textField.addEventListener("blur", normalBorder)
 
 
-console.log(save);
-console.log(textField);
+});
