4048e976669a66323634a6e6f06436b8629bacef
JamesOBlack <jamesoblack@fullsail.edu>
2013-05-13 11:57:49 -0400

    Having Issue with the makeField.
    I have set up the function and called it but it want show up when page
    loads.

--- js/main.js
+++ js/main.js
@@ -7,71 +7,82 @@ Project 2
 */
 window.addEventListener("DOMContentLoaded", function(){
    
-    localStorage.clear();
-    
-    // My get id function.
+// My get id function.
     function $(x) {
-        var elementId = document.getElementById(x);
-        return elementId;
+         var elementId = document.getElementById(x);
+         return elementId;
     }
-    
+         
     // Clear storage.
-        var storage   = $("clearStorage");
-        storage.addEventListener("click", clearStorage);
-    
-     var clearStorage = function() {
-        //confirm("Do you want to clear all data?")
-        localStorage.clear();
-        console.log("Clear worked");
+    var clearStorageLink   = $("clearStorage");
+    clearStorageLink.addEventListener("click", clearStorage);
+         
+    var clearStorage = function() {
+             
+    }
+// Make Select Field With js.
+    function makeField() {
+        var formTag = document.getElementByTagName("form"), // Array.
+            selectLi = $("select"),
+            makeSelect = document.createElement("select");
+            makeSelect.setAttribute("id", "groups");
+        for(var i=0, j=myTypeArray.length; i<j; i++){
+            var makeOption = document.createElement("option");
+            var optText = myTypeArray[i];
+            makeOption.setAttribute("value", optText);
+            makeOption.innerHTML = optText;
+            makeSelect.appendChild(makeOption);
+        }
+        selectLi.appendChild(makeSelect);
     }
     
-    // Show storage.
-        function showData(){
-            toggleControls("on");
-            var makeDiv = document.createElement("div");
-            makeDiv.setAttribute("id", "items");
-            var makeList = document.createElement("ul");
-            makeDiv.appendChild(makeList);
-            document.body.appendChild(makeDiv);
-            $("items").style.display = "block";
-            for (var i=0, len=localStorage.length; i,len; i++) {
-                 var makeLi = document.createElement("li");
-                 makeList.appendChild(makeLi);
-                 var key = localStorage.key(i);
-                 var value = localStorage.getItem(key);
-                 var obj = JSON.parse(value);
-                 var makeSublist = document.createElement("ul");
-                 makeLi.appendChild(makeSublist);
-                 for (var n in obj) {
-                      var makeSubli = document.createElement("li");
-                      makeSublist.appendChild(makeSubli);
-                      var optSubText = obj[n][0]+" "+obj[n][1];
-                      makeSubli.innerHTML = optSubText;
-                 }
+// Show storage.
+    function showData(){
+        toggleControls("on");
+        var makeDiv = document.createElement("div");
+        makeDiv.setAttribute("id", "items");
+        var makeList = document.createElement("ul");
+        makeDiv.appendChild(makeList);
+        document.body.appendChild(makeDiv);
+        $("items").style.display = "block";
+        for (var i=0, len=localStorage.length; i<len; i++) {
+            var makeLi = document.createElement("li");
+            makeList.appendChild(makeLi);
+            var key = localStorage.key(i);
+            var value = localStorage.getItem(key);
+            var obj = JSON.parse(value);
+            var makeSublist = document.createElement("ul");
+            makeLi.appendChild(makeSublist);
+            for (var n in obj) {
+                var makeSubli = document.createElement("li");
+                makeSublist.appendChild(makeSubli);
+                var optSubText = obj[n][0]+" "+obj[n][1];
+                makeSubli.innerHTML = optSubText;
             }
         }
-                var showStorage = $("displayData");
-                showStorage.addEventListener("click", showData);
+    }
+            var showStorage = $("displayData");
+            showStorage.addEventListener("click", showData);
         
-        function toggleControls(n) {
-            switch(n) {
-                case "on":
-                    $("choreForm").style.display = "none";
-                    $("clearStorage").style.display = "inline";
-                    $("displayData").style.display = "none";
-                    $("addNewChore").style.display = "inline";
-                    break;
-                case "off":
-                    $("choreForm").style.display = "block";
-                    $("clearStorage").style.display = "inline";
-                    $("displayData").style.display = "inline";
-                    $("addNewChore").style.display = "none";
-                    $("items").style.display = "none";
-                    break;
-                default:
-                    return false;
-            }
+    function toggleControls(n) {
+        switch(n) {
+            case "on":
+                $("choreForm").style.display = "none";
+                $("clearStorage").style.display = "inline";
+                $("displayData").style.display = "none";
+                $("addNewChore").style.display = "inline";
+                break;
+            case "off":
+                $("choreForm").style.display = "block";
+                $("clearStorage").style.display = "inline";
+                $("displayData").style.display = "inline";
+                $("addNewChore").style.display = "none";
+                $("items").style.display = "none";
+                break;
+            default:
+            return false;
         }
+    }
          
         function getSelectedRadio() {
             var radios = document.forms[0].difficulty
@@ -82,7 +93,7 @@ window.addEventListener("DOMContentLoaded", function(){
             }
         }
         
-    // Save data
+// Save data
         var saveButton     = $("saveButton");
         saveButton.addEventListener("click", saveData);
         function saveData() {
@@ -90,7 +101,7 @@ window.addEventListener("DOMContentLoaded", function(){
         getSelectedRadio();
         var item           = {}
             item.chore     = ["Chore", $("chore").value];
-            item.type      = ["Type", $("type").value];
+            //item.type      = ["Type", $("type").value];
             item.difficulty= ["difficulty", difficultyValue];
             item.range     = ["Range", $("range").value];
             item.choreDate = ["Chore Date", $("choreDate").value];
@@ -99,13 +110,14 @@ window.addEventListener("DOMContentLoaded", function(){
             alert("Chore Saved!");
     }
 
+        
+        
     
-    
-    
-    var myArray = [],
-    difficultyValue
-    ;
-   
+// My array for dropdown menu.
+        var myTypeArray = ["--Choose A Location--", "laundry", "kitchen", "bathroom", "living room", "beddroom", "yard"],
+        difficultyValue
+        ;
+        makeField();
     
     
 
