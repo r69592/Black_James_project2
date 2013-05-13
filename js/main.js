e6ef1a902481003c91ca8556348f5ece0e131c55
JamesOBlack <jamesoblack@fullsail.edu>
2013-05-12 20:04:50 -0400

    updated still having issue with show data function.

--- js/main.js
+++ js/main.js
@@ -6,6 +6,14 @@ Project 2
 
 */
 window.addEventListener("DOMContentLoaded", function(){
+   
+    localStorage.clear();
+    
+    // My get id function.
+    function $(x) {
+        var elementId = document.getElementById(x);
+        return elementId;
+    }
     
     // Clear storage.
         var storage   = $("clearStorage");
@@ -17,20 +25,15 @@ window.addEventListener("DOMContentLoaded", function(){
         console.log("Clear worked");
     }
     
-    
-    
-    
-    
     // Show storage.
-        var showStorage = $("displayData");
-        showStorage.addEventListener("click", showData);
-        
         function showData(){
+            toggleControls("on");
             var makeDiv = document.createElement("div");
             makeDiv.setAttribute("id", "items");
             var makeList = document.createElement("ul");
             makeDiv.appendChild(makeList);
             document.body.appendChild(makeDiv);
+            $("items").style.display = "block";
             for (var i=0, len=localStorage.length; i,len; i++) {
                  var makeLi = document.createElement("li");
                  makeList.appendChild(makeLi);
@@ -47,18 +50,48 @@ window.addEventListener("DOMContentLoaded", function(){
                  }
             }
         }
-    
+                var showStorage = $("displayData");
+                showStorage.addEventListener("click", showData);
+        
+        function toggleControls(n) {
+            switch(n) {
+                case "on":
+                    $("choreForm").style.display = "none";
+                    $("clearStorage").style.display = "inline";
+                    $("displayData").style.display = "none";
+                    $("addNewChore").style.display = "inline";
+                    break;
+                case "off":
+                    $("choreForm").style.display = "block";
+                    $("clearStorage").style.display = "inline";
+                    $("displayData").style.display = "inline";
+                    $("addNewChore").style.display = "none";
+                    $("items").style.display = "none";
+                    break;
+                default:
+                    return false;
+            }
+        }
+         
+        function getSelectedRadio() {
+            var radios = document.forms[0].difficulty
+            for(var i=0; i < radios.length; i++) {
+                if (radios[i].checked) {
+                     difficultyValue = radios[i].value;
+               }
+            }
+        }
+        
     // Save data
         var saveButton     = $("saveButton");
         saveButton.addEventListener("click", saveData);
         function saveData() {
         var id             = Math.floor(Math.random()*1000001);
+        getSelectedRadio();
         var item           = {}
             item.chore     = ["Chore", $("chore").value];
             item.type      = ["Type", $("type").value];
-            //item.easy      = ["Easy", $("easy").value];
-            //item.medium    = ["Medium", $("medium").value];
-            //item.hard      = ["Hard", $("hard").value];
+            item.difficulty= ["difficulty", difficultyValue];
             item.range     = ["Range", $("range").value];
             item.choreDate = ["Chore Date", $("choreDate").value];
             item.notes     = ["Notes", $("notes").value];
@@ -67,16 +100,12 @@ window.addEventListener("DOMContentLoaded", function(){
     }
 
     
-    // My get id function.
-    function $(x) {
-        var elementId = document.getElementById(x);
-        return elementId;
-    }
     
     
-    
+    var myArray = [],
+    difficultyValue
+    ;
    
-    //localStorage.clear();
     
     
 
